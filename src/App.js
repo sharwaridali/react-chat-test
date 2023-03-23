1226c70cf52704dfc57bd94f7dc475aa0c38e5e3
Aditya Wagh <adityawagh169@gmail.com>
2023-03-24 00:01:01 +0530

    Added Lazy Loading to ChatListItem and Box shadow

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import { useContext } from "react";
+import React, { Suspense, useContext } from "react";
 import { AuthContext } from "./context/AuthContext";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home";
-import Login from "./Pages/Login";
-import Register from "./Pages/Register";
+// import Home from "./Pages/Home";
+// import Login from "./Pages/Login";
+// import Register from "./Pages/Register";
 
 import "./styles/main.scss";
 import "./styles/form.scss";
-import Header from "./components/Header";
+import Loading from "./components/Loading";
+// import Header from "./components/Header";
+
+const Home = React.lazy(() => import("./Pages/Home"));
+const Login = React.lazy(() => import("./Pages/Login"));
+const Register = React.lazy(() => import("./Pages/Register"));
+const Header = React.lazy(() => import("./components/Header"));
 
 export default function App() {
   const { currentUser } = useContext(AuthContext);
@@ -30,37 +36,45 @@ export default function App() {
 
   return (
     <div className="container">
-      <Header />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/">
-            <Route
-              index
-              element={
-                <ProtectedRoute>
-                  <Home />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="register"
-              element={
-                <RedirectHomeRoute>
-                  <Register />
-                </RedirectHomeRoute>
-              }
-            />
-            <Route
-              path="login"
-              element={
-                <RedirectHomeRoute>
-                  <Login />
-                </RedirectHomeRoute>
-              }
-            />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Suspense fallback={<Loading />}>
+        <Header />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/">
+              <Route
+                index
+                element={
+                  <ProtectedRoute>
+                    <Suspense fallback={<Loading />}>
+                      <Home />
+                    </Suspense>
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="register"
+                element={
+                  <RedirectHomeRoute>
+                    <Suspense fallback={<Loading />}>
+                      <Register />
+                    </Suspense>
+                  </RedirectHomeRoute>
+                }
+              />
+              <Route
+                path="login"
+                element={
+                  <RedirectHomeRoute>
+                    <Suspense fallback={<Loading />}>
+                      <Login />
+                    </Suspense>
+                  </RedirectHomeRoute>
+                }
+              />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
     </div>
   );
 }
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,12 @@
 import { doc, onSnapshot } from "firebase/firestore";
-import React, { useContext, useEffect, useState } from "react";
+import React, { Suspense, useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import { db } from "../../Firebase";
-import ChatListItem from "./ChatListItem";
+import Loading from "../Loading";
+// import ChatListItem from "./ChatListItem";
 import Search from "./Search";
+
+const ChatListItem = React.lazy(() => import("./ChatListItem"));
 
 function Sidebar(props) {
   const { currentUser } = useContext(AuthContext);
@@ -35,24 +38,26 @@ function Sidebar(props) {
     <div className="sidebar-container">
       <Search toggleSidebar={props.toggleSidebar} />
       <div className="chat-list">
-        {userChats &&
-          Object.keys(userChats).length !== 0 &&
-          Object.entries(userChats)
-            ?.sort((a, b) => b[1].date - a[1].date)
-            .map((chat) => {
-              // console.log("userChats", chat);
-              return (
-                <ChatListItem
-                  key={chat[0]}
-                  avatar={chat[1].userInfo.photoURL}
-                  displayName={chat[1].userInfo.displayName}
-                  lastMessage={chat[1].lastMessage?.text}
-                  date={Date(chat[1].date)}
-                  userInfo={chat[1].userInfo}
-                  toggleSidebar={props.toggleSidebar}
-                />
-              );
-            })}
+        <Suspense fallback={<Loading />}>
+          {userChats &&
+            Object.keys(userChats).length !== 0 &&
+            Object.entries(userChats)
+              ?.sort((a, b) => b[1].date - a[1].date)
+              .map((chat) => {
+                // console.log("userChats", chat);
+                return (
+                  <ChatListItem
+                    key={chat[0]}
+                    avatar={chat[1].userInfo.photoURL}
+                    displayName={chat[1].userInfo.displayName}
+                    lastMessage={chat[1].lastMessage?.text}
+                    date={Date(chat[1].date)}
+                    userInfo={chat[1].userInfo}
+                    toggleSidebar={props.toggleSidebar}
+                  />
+                );
+              })}
+        </Suspense>
 
         {userChats && Object.keys(userChats).length === 0 && (
           <div className="tip-chat-list">
